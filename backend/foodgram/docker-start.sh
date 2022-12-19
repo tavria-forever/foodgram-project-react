1bd1076819df358bf92d7ced85cf218705d1464e
Nickolay Ilchenko <yndx.tavria@yandex.ru>
2022-12-20 01:02:07 +0300

    Подготовка к деплою в облаке

--- backend/foodgram/docker-start.sh
+++ backend/foodgram/docker-start.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
 python3 manage.py migrate
-#python3 manage.py loaddata fixtures.json
-python manage.py initadmin
-#python3 manage.py collectstatic --no-input
-gunicorn api_yamdb.wsgi:application --bind 0:8000
+python3 manage.py loaddata fixtures.json
+python3 manage.py collectstatic --no-input
+gunicorn foodgram.wsgi:application --bind 0:8000
